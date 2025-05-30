f7ced124d279dc78a17fc5f7b768f7857c2afed4
Coen Mooij <coenmooij@gmail.com>
2025-05-30 14:48:21 +0200

    add form, fix button and title gradients

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { BarComponent, ButtonComponent, InfoComponent, SectionComponent } from '../../components';
 import { RandomizeService } from '../../core';
-import { NgForOf, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 
 @Component({
   templateUrl: 'home.page.html',
@@ -15,5 +15,11 @@ export class HomePage {
 
   public onRandomize(): void {
     this.randomResults = this.randomizeService.randomize();
+  }
+
+  public onReview(): void {
+    const formUrl = 'https://forms.gle/8xNU7m9nkTuQ496z5';
+
+    window.open(formUrl, '_blank');
   }
 }
